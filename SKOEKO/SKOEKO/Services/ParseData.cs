f97a32552661ff1a3484b39955953a2490cb67d4
Michał <michalyjp@gmail.com>
2020-12-21 10:38:24 +0100

    DIRs also work

--- SKOEKO/SKOEKO/Services/ParseData.cs
+++ SKOEKO/SKOEKO/Services/ParseData.cs
@@ -491,5 +491,100 @@ namespace SKOEKO.Services
 
             return datatable;
         }
+        /// <summary>
+        /// A method that parse DIR data for daily raport exported to excel
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public DataTable ParseMonthDataDirExcel(SqlDataReader reader)
+        {
+            // Averages
+            float averageMax = 0;
+            float averageMin = 0;
+
+            // Sums
+            float sumMin = 0;
+            float sumMax = 0;
+
+            // Sonstiges :)
+            string unit = "g/l";
+            int nrDnia = 1;
+
+            // Create datatable
+            DataTable datatable = new DataTable();
+
+            // Create columns
+            DataColumn idColumn = new DataColumn("ID", typeof(Int32));
+            DataColumn nrDniaColumn = new DataColumn("Nr Dnia", typeof(int));
+            DataColumn dataColumn = new DataColumn("Data", typeof(String));
+            DataColumn maxColumn = new DataColumn("Max Wartość", typeof(float));
+            DataColumn minColumn = new DataColumn("Min Wartość", typeof(float));
+            DataColumn jednostkaColumn = new DataColumn("Jednostka", typeof(String));
+
+            // Add columns do datatable
+            datatable.Columns.Add(idColumn);
+            datatable.Columns.Add(nrDniaColumn);
+            datatable.Columns.Add(dataColumn);
+            datatable.Columns.Add(maxColumn);
+            datatable.Columns.Add(minColumn);
+            datatable.Columns.Add(jednostkaColumn);
+
+            // Add a row
+            DataRow dataRow;
+
+            // Read the reader
+            while (reader.Read())
+            {
+
+                // Get the data
+                DateTime data = reader.GetDateTime(2);
+                string Max = reader.GetString(0);
+                string Min = reader.GetString(1);
+                int Id = reader.GetInt32(3);
+
+                // Parse the data
+                DateTime parsedData = data.AddDays(-1);
+                String reparsedData = parsedData.ToString("yyyy-MM-dd");
+                float parsedMax = float.Parse(Max);
+                float parsedMin = float.Parse(Min);
+
+
+                // Calculate average and sum
+                sumMin += parsedMin;
+                sumMax += parsedMax;
+
+                // Assign data to the row
+                dataRow = datatable.NewRow();
+                dataRow["ID"] = Id;
+                dataRow["Nr Dnia"] = nrDnia;
+                dataRow["Data"] = reparsedData;
+                dataRow["Max Wartość"] = parsedMax;
+                dataRow["Min Wartość"] = parsedMin;
+                dataRow["Jednostka"] = unit;
+
+                // Add row to the datatable
+                datatable.Rows.Add(dataRow);
+
+                // Increase a number of the day
+                nrDnia++;
+            }
+
+            // Calculate the averages
+            averageMax = sumMax / (nrDnia - 1);
+            averageMin = sumMin / (nrDnia - 1);
+
+
+            // Add row that consist of sum and average
+            dataRow = datatable.NewRow();
+            dataRow["Data"] = "Średnia";
+            dataRow["Max Wartość"] = averageMax;
+            dataRow["Min Wartość"] = averageMin;
+            dataRow["Jednostka"] = unit;
+
+            // Add row to the datatable
+            datatable.Rows.Add(dataRow);
+
+            return datatable;
+        }
     }
 }
